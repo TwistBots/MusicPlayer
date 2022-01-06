ba4111fd55e40a95b13823a3a1ce9ac346de7d90
Simple Boy <96994809+TwistBots@users.noreply.github.com>
2022-01-06 10:32:15 +0530

    Update decorators.py

--- core/decorators.py
+++ core/decorators.py
@@ -1,6 +1,6 @@
 """
 Music Player, Telegram Voice Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
+Copyright (c) 2021  Twist Bots <https://github.com/TwistBots>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
@@ -98,7 +98,7 @@ def handle_error(func: Callable) -> Callable:
         try:
             return await func(client, obj, *args)
         except Exception:
-            k = "AsmSafone"
+            k = "TwistBots"
             id = int(time.time())
             date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             error_msg = await pyro_client.send_message(
